8c61a8150bc5d7da8d9209b9054b1b34eb96132c
wmsales <salesjerez@gmail.com>
2024-11-15 17:33:39 -0600

    update: add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 function ColorCelda($status) {
     switch ($status) {
         case 'Disponible':
-            return "#198754"; // Verde (Disponible)
+            return "#442b26"; // Verde (Disponible)
         case 'Reservado':
             return "#ffc107"; // Amarillo (Reservado)
         case 'No disponible':
@@ -31,74 +31,75 @@ function ColorCelda($status) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Laravel</title>
 </head>
 
 <body>
     <div style="height: 100vh" class="w-100 bg-light d-flex justify-content-center align-items-center">
         <div>
-            <p class="display-5 text-center">Bus</p>
-            <table style="width: 450px;" class="table table-bordered">
+            <p class="display-5 text-center">Reserva tu lugar</p>
+            <img src="svg.svg" alt="">
+            <table style="width: 400px;" class="table table-bordered">
                 <thead>
                     <tr class="">
-                        <th style="background-color: black; color:white; text-align:center" scope="col">1</th>
-                        <th style="background-color: black; color:white; text-align:center" scope="col">2</th>
-                        <th style="background-color: black; color:white; text-align:center" scope="col">3</th>
-                        <th style="background-color: #424242; color:white; text-align:center" scope="col">4</th>
-                        <th style="background-color: #424242; color:white; text-align:center" scope="col">5</th>
-                        <th style="background-color: #424242; color:white; text-align:center" scope="col">6</th>
+                        <th style="background-color: purple; color:white; text-align:center" scope="col">1</th>
+                        <th style="background-color: purple; color:white; text-align:center" scope="col">2</th>
+                        <th style="background-color: purple; color:white; text-align:center" scope="col">3</th>
+                        <th style="background-color: green; color:white; text-align:center" scope="col">4</th>
+                        <th style="background-color: green; color:white; text-align:center" scope="col">5</th>
+                        <th style="background-color: green; color:white; text-align:center" scope="col">6</th>
                     </tr>
                 </thead>
-                <tbody>
-
+                <tbody class="table-group-divider">
                     <tr>
                         @foreach ($registrosA as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosB as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosC as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosD as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosE as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosF as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosG as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosH as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosI as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
                     <tr>
                         @foreach ($registrosJ as $reg)
-                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}</th>
+                        <th style="background-color: {{ ColorCelda($reg->estado) }}" class="text-center">{{ $reg->asiento }}<i class="bi bi-person text-white"></i></th>
                         @endforeach
                     </tr>
 
